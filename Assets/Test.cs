d11aa58d06011563b9cff4a7222194de69a7a326
kenji.takada <falcon9628@yahoo.co.jp>
2020-12-19 06:18:57 +0900

    配列を宣言して出力+BossClassの構築
    int型の配列arrayを宣言、for文にて連続的表示(順次+逆順)　Console表示可に
    BossClassを作成し、変数mpを宣言と魔法攻撃をするMagic関数の定義、Magic関数を10回使用する設定　Console表示可に

---
+++ Assets/Test.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+
+public class Boss
+{
+    private int hp = 100;   // 体力
+    private int power = 25; // 攻撃力
+
+    private int mp = 53;   //魔法力
+
+
+
+    // 攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    // 防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+
+    //魔法攻撃用の関数
+    public void Magic(int use_mp)
+    {
+        this.mp -= use_mp;
+        Debug.Log("魔法攻撃をした。残りMPは"+this.mp+"。");
+
+        if (this.mp < 5){
+            Debug.Log("MPが足りないため魔法が使えない。");            
+        } 
+
+
+    }
+
+}
+
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+        //配列の宣言
+        int[] array = { 11, 22, 33, 44, 55 };
+
+        //配列の順次表示
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+
+        }
+
+        //配列の逆順次表示
+        for (int i = array.Length-1; 0 <= i; i--)
+        {
+            Debug.Log(array[i]);
+
+        }
+
+
+        //Class実行
+        //Instance化
+        Boss battle = new Boss();
+
+        //Class 関数　各種実行
+        battle.Attack();
+        battle.Defence(3);
+
+        battle.Magic(5);
+        battle.Magic(5);
+        battle.Magic(5);
+        battle.Magic(5);
+        battle.Magic(5);
+        battle.Magic(5);
+        battle.Magic(5);
+        battle.Magic(5);
+        battle.Magic(5);
+        battle.Magic(5);
+
+        battle.Attack();
+        battle.Defence(3);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
